e09e3754b77c567918e0d400b115ac9ae1dae610
Fonov Sergei <ask@mounlion.com>
2018-02-24 20:01:57 +0300

    Подключил бустрап 4 как сас стили для быстрого редактирования

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from './components/index';
 //CSS
-import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/boostrap/bootstrap.css'
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
