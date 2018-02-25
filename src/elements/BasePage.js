b1262cf39338dc86a72e3e8b0c15206efd9977d1
Fonov Sergei <ask@mounlion.com>
2018-02-25 13:01:25 +0300

    Переписал боковое меню и главный контейнер на САСС

--- src/elements/BasePage.js
+++ src/elements/BasePage.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {LeftPanel, TopPanel} from './index'
-import { StyleSheet, css } from 'aphrodite';
 
 
 export default class BasePage extends Component{
@@ -10,44 +9,13 @@ export default class BasePage extends Component{
         const {children} = this.props;
 
         return (
-            <div className={css(styles.container_fluid)}>
-                <LeftPanel />
+            <div className='it-container'>
+                <LeftPanel my_orders_badge={2} />
                 <TopPanel />
-                <div className={css(styles.main)}>
+                <div className='it-page'>
                     {children}
                 </div>
             </div>
         )
     }
 }
-
-const coolFont = {
-    fontFamily: "Avenir Next",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    src: `url('${require('../assets/fonts/Avenir Next Regular.otf')}')`
-};
-
-const styles = StyleSheet.create({
-   container_fluid: {
-       width: '100%',
-       minWidth: 1024,
-       padding: 0,
-       margin: 0,
-       height: '100%',
-       minHeight: 800,
-       fontFamily: [coolFont, "sans-serif"],
-       fontSize: 14,
-       color: '#1b1f2b',
-       backgroundColor: 'rgba(40, 46, 61, 0.07);'
-   },
-   main: {
-       position: 'relative',
-       marginLeft: 94,
-       backgroundColor: '#ffffff',
-       height: 'auto',
-       borderTopLeftRadius: 5,
-       boxShadow: '-1px -1px 8px 0 rgba(0, 0, 0, .2);',
-       paddingLeft: '2em'
-   }
-});
--- src/elements/LeftPanel.js
+++ src/elements/LeftPanel.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { StyleSheet, css } from 'aphrodite';
 import FontAwesome from 'react-fontawesome'
 import { Badge } from 'reactstrap';
 
@@ -8,13 +7,13 @@ export default class LeftPanel extends Component{
 
     render() {
 
-        const {} = this.props;
+        const { my_orders_badge = 3 } = this.props;
 
         return (
-            <div className={css(styles.menu)}>
+            <div className='it-left-panel'>
                 <div className='text-center mt-2'>
                     <img
-                        className={css(styles.logo)}
+                        className='logo'
                         src={require('../assets/logo.png')}
                         alt='logo'
                     />
@@ -23,41 +22,43 @@ export default class LeftPanel extends Component{
                 <div className='text-center mt-4'>
                     <div className="d-flex flex-column">
                         <div>
-                            <Badge pill color='#3454d1' className={css(styles.badge_icon)}>3</Badge>
+                            <Badge pill color='primary' className='ml-4'>
+                                {my_orders_badge}
+                            </Badge>
                         </div>
                         <div>
-                            <FontAwesome name='long-arrow-down' className={css(styles.icon)}/>
-                            <FontAwesome name='long-arrow-up' className={css(styles.icon)}/>
+                            <FontAwesome name='long-arrow-down' className='icon' />
+                            <FontAwesome name='long-arrow-up' className='icon' />
                             <p>My orders</p>
                         </div>
                     </div>
                 </div>
 
                 <div className='text-center'>
-                    <img src={require('../assets/icons/wallet.svg')} className={css(styles.custom_icon)}/>
+                    <img src={require('../assets/icons/wallet.svg')} className='img_icon' />
                     <p>Wallets</p>
                 </div>
 
                 <div className='text-center mt-4'>
-                    <FontAwesome name='pie-chart' className={css(styles.icon)}/>
+                    <FontAwesome name='pie-chart' className='icon'/>
                     <p>Analytics</p>
                 </div>
 
                 <div className='text-center mt-4'>
-                    <FontAwesome name='users' className={css(styles.icon)}/>
+                    <FontAwesome name='users' className='icon'/>
                     <p>Traders</p>
                 </div>
 
                 <div className='text-center mt-4'>
-                    <FontAwesome name='comment' className={css(styles.icon)}/>
+                    <FontAwesome name='comment' className='icon'/>
                     <p>Messages</p>
                 </div>
 
-                <div className={`${css(styles.footer)} text-center`}>
-                    <div className={css(styles.small)}>
+                <div className='footer text-center'>
+                    <div className='it-fs12'>
                         15:48
                     </div>
-                    <div className={css(styles.small_with_opacity)}>
+                    <div className='it-fs12 it-half-opacity'>
                         UTC +3 <FontAwesome name='caret-down ' />
                     </div>
                 </div>
@@ -65,43 +66,3 @@ export default class LeftPanel extends Component{
         )
     }
 }
-
-const styles = StyleSheet.create({
-    menu: {
-        width: 94,
-        height: '100%',
-        backgroundColor: '#f5f7fa',
-        position: 'absolute'
-    },
-    logo: {
-        width: 30
-    },
-    footer: {
-        position: 'absolute',
-        bottom: 10,
-        width: '100%',
-        height: 'auto'
-    },
-    custom_icon: {
-        width: 22,
-        opacity: 0.8
-    },
-    icon: {
-        fontSize: 22,
-        opacity: 0.8
-    },
-    small: {
-        fontSize: 12,
-        color: '#282e3d'
-    },
-    small_with_opacity: {
-        fontSize: 12,
-        color: '#282e3d',
-        opacity: 0.6
-    },
-    badge_icon: {
-        marginLeft: '2.5em',
-        backgroundColor: '#3454d1',
-        color: 'white'
-    }
-});
