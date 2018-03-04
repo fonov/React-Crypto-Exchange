4b4283cb8d4f084ad349f721f0f6f19d8fd9ff6e
Fonov Sergei <ask@mounlion.com>
2018-03-04 14:54:22 +0300

    Markets Tile

--- src/components/markets_list.js
+++ src/components/markets_list.js
@@ -9,9 +9,23 @@ import {push} from "react-router-redux";
 
 
 class MarketList extends Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            tile: true
+        }
+    }
+
+    changeView() {
+        this.setState({tile: !this.state.tile})
+    }
+
     render() {
 
-        const {push} = this.props;
+        const {push} = this.props,
+            {tile} = this.state;
 
         return (
             <BasePage
@@ -55,65 +69,173 @@ class MarketList extends Component{
                                     </span>
                                 </div>
                             </div>
-                            <div className='ml-4 icon-fa mr-4'>
+                            <div className={`ml-4 icon-fa mr-4 ${tile ? 'non_active' : ''}`} onClick={this.changeView.bind(this)}>
                                 <FontAwesome name='bars' />
                             </div>
-                            <div className='tile non_active'>
+                            <div className={`tile ${!tile ? 'non_active' : ''}`} onClick={this.changeView.bind(this)}>
                                 <img src={require('../assets/icons/tile.svg')} />
                             </div>
                         </div>
                     </div>
-                    <Table className='border-bottom'>
-                        <thead>
-                        <tr>
-                            <th><span className='it-dashed'>Coin</span></th>
-                            <th>Pair</th>
-                            <th><span className='it-dashed'>Last price</span></th>
-                            <th><span className='it-dashed'>High</span></th>
-                            <th><span className='it-dashed'>Low</span></th>
-                            <th><span className='it-dashed text-primary border-primary'>Volume</span> <FontAwesome name='sort-desc'/></th>
-                            <th><span className='it-dashed'>% 1h</span></th>
-                            <th><span className='it-dashed'>% 1d</span></th>
-                            <th><span className='it-dashed'>% 1w</span></th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            [1,2,3,4,5,6,7,8].map(item => (
-                                <tr key={item}>
-                                    <td className='with_img'>
-                                        <img src='https://chain.so/Bitcoin@2x.png' />
-                                        <strong className='it-fs16 ml-3'>Litecoin</strong>
-                                    </td>
-                                    <td>
-                                        <strong className='it-fs16 text-primary'>LTC/BTC</strong>
-                                    </td>
-                                    <td>
-                                        <span className='text-success it-fs16 it-medium'><FontAwesome name='sort-asc'/> 0.01810002</span>
-                                    </td>
-                                    <td>
-                                        <span className='it-fs16'>0.01810002</span>
-                                    </td>
-                                    <td>
-                                        <span className='it-fs16'>0.01810002</span>
-                                    </td>
-                                    <td>
-                                        <span className='it-fs16'>0.01810002</span>
-                                    </td>
-                                    <td>
-                                        <span className='text-success it-fs16 it-medium'>+ 23%</span>
-                                    </td>
-                                    <td>
-                                        <span className='text-success it-fs16 it-medium'>+ 23%</span>
-                                    </td>
-                                    <td>
-                                        <span className='text-danger it-fs16 it-medium'>- 23%</span>
-                                    </td>
+                    {
+                        tile ? (
+                            <div className='tile-block'>
+                                <Row>
+                                    <Col className='col-4'>
+                                        <Card>
+                                            <CardBody>
+                                                <div className='d-flex justify-content-between'>
+                                                    <strong className='it-fs24'>LTC/BTC</strong>
+                                                    <div className='it-medium it-fs16'>
+                                                        <Badge color='success' pill>+24.7%</Badge>
+                                                    </div>
+                                                </div>
+                                                <span className='text-success it-fs16 it-medium'><FontAwesome name='sort-asc'/> 0.01810002</span>
+                                                <div className='d-flex justify-content-around'>
+                                                    <div className='text-center'>
+                                                        <span className='it-fs12 it-half-opacity'>Bid</span>
+                                                        <div>
+                                                            <span className='it-fs18'>0.01799996</span>
+                                                        </div>
+                                                    </div>
+                                                    <div className='text-center border-left border-right px-4 mx-2'>
+                                                        <span className='it-fs12 it-half-opacity'>Spread</span>
+                                                        <div>
+                                                            <span className='it-fs18'>0.01799996</span>
+                                                        </div>
+                                                    </div>
+                                                    <div className='text-center'>
+                                                        <span className='it-fs12 it-half-opacity'>Ask</span>
+                                                        <div>
+                                                            <span className='it-fs18'>0.01799996</span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </CardBody>
+                                        </Card>
+                                    </Col>
+                                    <Col className='col-4'>
+                                        <Card>
+                                            <CardBody>
+                                                <div className='d-flex justify-content-between'>
+                                                    <strong className='it-fs24'>LTC/BTC</strong>
+                                                    <div className='it-medium it-fs16'>
+                                                        <Badge color='success' pill>+24.7%</Badge>
+                                                    </div>
+                                                </div>
+                                                <span className='text-success it-fs16 it-medium'><FontAwesome name='sort-asc'/> 0.01810002</span>
+                                                <div className='d-flex justify-content-around'>
+                                                    <div className='text-center'>
+                                                        <span className='it-fs12 it-half-opacity'>Bid</span>
+                                                        <div>
+                                                            <span className='it-fs18'>0.01799996</span>
+                                                        </div>
+                                                    </div>
+                                                    <div className='text-center border-left border-right px-4 mx-2'>
+                                                        <span className='it-fs12 it-half-opacity'>Spread</span>
+                                                        <div>
+                                                            <span className='it-fs18'>0.01799996</span>
+                                                        </div>
+                                                    </div>
+                                                    <div className='text-center'>
+                                                        <span className='it-fs12 it-half-opacity'>Ask</span>
+                                                        <div>
+                                                            <span className='it-fs18'>0.01799996</span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </CardBody>
+                                        </Card>
+                                    </Col>
+                                    <Col className='col-4'>
+                                        <Card>
+                                            <CardBody>
+                                                <div className='d-flex justify-content-between'>
+                                                    <strong className='it-fs24'>LTC/BTC</strong>
+                                                    <div className='it-medium it-fs16'>
+                                                        <Badge color='danger' pill>+24.7%</Badge>
+                                                    </div>
+                                                </div>
+                                                <span className='text-danger it-fs16 it-medium'><FontAwesome name='sort-asc'/> 0.01810002</span>
+                                                <div className='d-flex justify-content-around'>
+                                                    <div className='text-center'>
+                                                        <span className='it-fs12 it-half-opacity'>Bid</span>
+                                                        <div>
+                                                            <span className='it-fs18'>0.01799996</span>
+                                                        </div>
+                                                    </div>
+                                                    <div className='text-center border-left border-right px-4 mx-2'>
+                                                        <span className='it-fs12 it-half-opacity'>Spread</span>
+                                                        <div>
+                                                            <span className='it-fs18'>0.01799996</span>
+                                                        </div>
+                                                    </div>
+                                                    <div className='text-center'>
+                                                        <span className='it-fs12 it-half-opacity'>Ask</span>
+                                                        <div>
+                                                            <span className='it-fs18'>0.01799996</span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </CardBody>
+                                        </Card>
+                                    </Col>
+                                </Row>
+                            </div>
+                        ) : (
+                            <Table className='border-bottom'>
+                                <thead>
+                                <tr>
+                                    <th><span className='it-dashed'>Coin</span></th>
+                                    <th>Pair</th>
+                                    <th><span className='it-dashed'>Last price</span></th>
+                                    <th><span className='it-dashed'>High</span></th>
+                                    <th><span className='it-dashed'>Low</span></th>
+                                    <th><span className='it-dashed text-primary border-primary'>Volume</span> <FontAwesome name='sort-desc'/></th>
+                                    <th><span className='it-dashed'>% 1h</span></th>
+                                    <th><span className='it-dashed'>% 1d</span></th>
+                                    <th><span className='it-dashed'>% 1w</span></th>
                                 </tr>
-                            ))
-                        }
-                        </tbody>
-                    </Table>
+                                </thead>
+                                <tbody>
+                                {
+                                    [1,2,3,4,5,6,7,8].map(item => (
+                                        <tr key={item}>
+                                            <td className='with_img'>
+                                                <img src='https://chain.so/Bitcoin@2x.png' />
+                                                <strong className='it-fs16 ml-3'>Litecoin</strong>
+                                            </td>
+                                            <td>
+                                                <strong className='it-fs16 text-primary'>LTC/BTC</strong>
+                                            </td>
+                                            <td>
+                                                <span className='text-success it-fs16 it-medium'><FontAwesome name='sort-asc'/> 0.01810002</span>
+                                            </td>
+                                            <td>
+                                                <span className='it-fs16'>0.01810002</span>
+                                            </td>
+                                            <td>
+                                                <span className='it-fs16'>0.01810002</span>
+                                            </td>
+                                            <td>
+                                                <span className='it-fs16'>0.01810002</span>
+                                            </td>
+                                            <td>
+                                                <span className='text-success it-fs16 it-medium'>+ 23%</span>
+                                            </td>
+                                            <td>
+                                                <span className='text-success it-fs16 it-medium'>+ 23%</span>
+                                            </td>
+                                            <td>
+                                                <span className='text-danger it-fs16 it-medium'>- 23%</span>
+                                            </td>
+                                        </tr>
+                                    ))
+                                }
+                                </tbody>
+                            </Table>
+                        )
+                    }
                 </div>
             </BasePage>
         )
