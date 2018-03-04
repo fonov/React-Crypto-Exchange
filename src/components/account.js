db5ff3235d8d438c9b5e699b6f9ad2b2569938f9
Fonov Sergei <ask@mounlion.com>
2018-03-04 22:11:54 +0300

    security

--- src/components/account.js
+++ src/components/account.js
@@ -10,9 +10,19 @@ import {push} from "react-router-redux";
 
 class Account extends Component{
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            account: false,
+            security: true
+        }
+    }
+
     render() {
 
-        const {push} = this.props;
+        const {push} = this.props,
+            {account, security} = this.state;
 
         return (
             <BasePage
@@ -37,11 +47,11 @@ class Account extends Component{
                                     <div className='active_line' />
                                     <FontAwesome name='home' className='it-fs18'/> <span className='it-fs14 it_light_opacity text'>Overview</span>
                                 </ListGroupItem>
-                                <ListGroupItem className='text_with_icon active_tab'>
+                                <ListGroupItem className={`text_with_icon ${account ? 'active_tab': ''}`}>
                                     <div className='active_line' />
                                     <FontAwesome name='user' className='it-fs18'/> <span className='it-fs14 it_light_opacity text'>Account</span>
                                 </ListGroupItem>
-                                <ListGroupItem className='text_with_icon'>
+                                <ListGroupItem className={`text_with_icon ${security ? 'active_tab': ''}`}>
                                     <img src={require('../assets/icons/security.svg')} /> <span className='it-fs14 it_light_opacity text'>Security</span>
                                 </ListGroupItem>
                                 <ListGroupItem className='text_with_icon'>
@@ -64,108 +74,139 @@ class Account extends Component{
                                 <FontAwesome name='sign-out' className='it-fs18'/> <span className='it-fs14 ml-2'>Logout</span>
                             </p>
                         </Col>
-                        <Col className='col-7 account'>
-                            <strong className='it-fs28'>Account</strong>
-                            <div>
-                                <div className='personal_information'>
-                                    <strong className='it-fs16'>Personal information</strong>
-                                    <Row className='mt-4'>
-                                        <Col className='col-6'>
-                                            <FormGroup>
-                                                <Label for="exampleEmail">First name</Label>
-                                                <Input type="email" name="email" id="exampleEmail" placeholder="Eddie" />
-                                            </FormGroup>
-                                        </Col>
-                                        <Col className='col-6'>
-                                            <FormGroup>
-                                                <Label for="exampleEmail">Last name</Label>
-                                                <Input type="email" name="email" id="exampleEmail" placeholder="Becker" />
-                                            </FormGroup>
-                                        </Col>
-                                    </Row>
-                                </div>
-                                <FormGroup>
-                                    <Label for="exampleText">Bio</Label>
-                                    <Input type="textarea" name="text" id="exampleText" />
-                                </FormGroup>
-                                <Row className='mt-4'>
-                                    <Col className='col-6'>
+                        {
+                            account ? [<Col className='col-7 account'>
+                                    <strong className='it-fs28'>Account</strong>
+                                    <div>
+                                        <div className='personal_information'>
+                                            <strong className='it-fs16'>Personal information</strong>
+                                            <Row className='mt-4'>
+                                                <Col className='col-6'>
+                                                    <FormGroup>
+                                                        <Label for="exampleEmail">First name</Label>
+                                                        <Input type="email" name="email" id="exampleEmail" placeholder="Eddie" />
+                                                    </FormGroup>
+                                                </Col>
+                                                <Col className='col-6'>
+                                                    <FormGroup>
+                                                        <Label for="exampleEmail">Last name</Label>
+                                                        <Input type="email" name="email" id="exampleEmail" placeholder="Becker" />
+                                                    </FormGroup>
+                                                </Col>
+                                            </Row>
+                                        </div>
                                         <FormGroup>
-                                            <Label for="exampleEmail">Username</Label>
-                                            <Input type="email" name="email" id="exampleEmail" placeholder="EddieBecker" />
+                                            <Label for="exampleText">Bio</Label>
+                                            <Input type="textarea" name="text" id="exampleText" />
                                         </FormGroup>
-                                    </Col>
-                                    <Col className='col-6'>
-                                        <FormGroup>
-                                            <Label for="exampleEmail">Country</Label>
-                                            <Input type="select" name="select" id="exampleSelect">
-                                                <option> <FontAwesome name='ban' />  United States</option>
-                                            </Input>
-                                        </FormGroup>
-                                    </Col>
-                                </Row>
-                                <Button color='light' className='border it-fs14' block>
-                                    <strong>Save</strong>
-                                </Button>
-                                <hr/>
-                                <Row className='other-setting'>
-                                    <Col className='col-6 border-right'>
-                                        <strong className='it-fs16'>Email</strong>
-                                        <div className='mt-3'>
-                                            <span className='it-fs14'>my.new.email@gmail.com</span>
+                                        <Row className='mt-4'>
+                                            <Col className='col-6'>
+                                                <FormGroup>
+                                                    <Label for="exampleEmail">Username</Label>
+                                                    <Input type="email" name="email" id="exampleEmail" placeholder="EddieBecker" />
+                                                </FormGroup>
+                                            </Col>
+                                            <Col className='col-6'>
+                                                <FormGroup>
+                                                    <Label for="exampleEmail">Country</Label>
+                                                    <Input type="select" name="select" id="exampleSelect">
+                                                        <option> <FontAwesome name='ban' />  United States</option>
+                                                    </Input>
+                                                </FormGroup>
+                                            </Col>
+                                        </Row>
+                                        <Button color='light' className='border it-fs14' block>
+                                            <strong>Save</strong>
+                                        </Button>
+                                        <hr/>
+                                        <Row className='other-setting'>
+                                            <Col className='col-6 border-right'>
+                                                <strong className='it-fs16'>Email</strong>
+                                                <div className='mt-3'>
+                                                    <span className='it-fs14'>my.new.email@gmail.com</span>
+                                                </div>
+                                                <FormGroup className='mt-4'>
+                                                    <Label for="exampleEmail">Email</Label>
+                                                    <Input type="email" name="email" id="exampleEmail" />
+                                                </FormGroup>
+                                                <div className='mb-4'>
+                                                    <Button color='light' className='border it-fs14 mt-4' block>
+                                                        <strong>Add email</strong>
+                                                    </Button>
+                                                </div>
+                                                <div className='password'>
+                                                    <div className='mb-4'>
+                                                        <strong className='it-fs16'>Password</strong>
+                                                    </div>
+                                                    <FormGroup>
+                                                        <Label for="exampleEmail">Password</Label>
+                                                        <Input type="password" name="email" id="exampleEmail" />
+                                                        <Label for="exampleEmail" className='mt-4'>Password confirmation</Label>
+                                                        <Input type="password" name="email" id="exampleEmail" />
+                                                    </FormGroup>
+                                                    <Button color='light' className='border it-fs14 mt-4' block>
+                                                        <strong>Change password</strong>
+                                                    </Button>
+                                                </div>
+                                            </Col>
+                                            <Col className='col-6'>
+                                                <strong className='it-fs16'>Language</strong>
+                                                <FormGroup className='mt-4'>
+                                                    <Input type="select" name="select" id="exampleSelect">
+                                                        <option>English</option>
+                                                    </Input>
+                                                </FormGroup>
+                                                <div className='timezone'>
+                                                    <strong className='it-fs16'>Timezone</strong>
+                                                    <FormGroup className='mt-4'>
+                                                        <Input type="select" name="select" id="exampleSelect">
+                                                            <option>GTM +3 Moscow</option>
+                                                        </Input>
+                                                    </FormGroup>
+                                                </div>
+                                            </Col>
+                                        </Row>
+                                    </div>
+                                </Col>,
+                                <Col className='col-2 text-center avatar'>
+                                    <img src='http://via.placeholder.com/108x108' className='rounded-circle'/>
+                                    <Button color='light' className='border it-fs14 mt-4' block>
+                                        <strong>Edit avatar</strong>
+                                    </Button>
+                                    <div className='mt-4 it-fs14 text-danger'>
+                                        <FontAwesome name='ban' /> <span>Delete account</span>
+                                    </div>
+                                </Col>] : null
+                        }
+                        {
+                            security ? (
+                                <Col className='col-9 base-account security'>
+                                    <strong className='it-fs28'>Security</strong>
+                                    <div className='d-flex flex-row'>
+                                        <div className="d-flex flex-row it-btn-group mt-4">
+                                            <div className="it-btn active">Login history</div>
+                                            <div className="it-btn"><span>Active sessions</span></div>
+                                            <div className="it-btn it-btn_m"><span>Two-step authentication</span></div>
                                         </div>
-                                        <FormGroup className='mt-4'>
-                                            <Label for="exampleEmail">Email</Label>
-                                            <Input type="email" name="email" id="exampleEmail" />
-                                        </FormGroup>
-                                        <div className='mb-4'>
-                                            <Button color='light' className='border it-fs14 mt-4' block>
-                                                <strong>Add email</strong>
-                                            </Button>
-                                        </div>
-                                        <div className='password'>
-                                            <div className='mb-4'>
-                                                <strong className='it-fs16'>Password</strong>
-                                            </div>
-                                            <FormGroup>
-                                                <Label for="exampleEmail">Password</Label>
-                                                <Input type="password" name="email" id="exampleEmail" />
-                                                <Label for="exampleEmail" className='mt-4'>Password confirmation</Label>
-                                                <Input type="password" name="email" id="exampleEmail" />
-                                            </FormGroup>
-                                            <Button color='light' className='border it-fs14 mt-4' block>
-                                                <strong>Change password</strong>
-                                            </Button>
-                                        </div>
-                                    </Col>
-                                    <Col className='col-6'>
-                                        <strong className='it-fs16'>Language</strong>
-                                        <FormGroup className='mt-4'>
-                                            <Input type="select" name="select" id="exampleSelect">
-                                                <option>English</option>
-                                            </Input>
-                                        </FormGroup>
-                                        <div className='timezone'>
-                                            <strong className='it-fs16'>Timezone</strong>
-                                            <FormGroup className='mt-4'>
-                                                <Input type="select" name="select" id="exampleSelect">
-                                                    <option>GTM +3 Moscow</option>
-                                                </Input>
-                                            </FormGroup>
-                                        </div>
-                                    </Col>
-                                </Row>
-                            </div>
-                        </Col>
-                        <Col className='col-2 text-center avatar'>
-                            <img src='http://via.placeholder.com/108x108' className='rounded-circle'/>
-                            <Button color='light' className='border it-fs14 mt-4' block>
-                                <strong>Edit avatar</strong>
-                            </Button>
-                            <div className='mt-4 it-fs14 text-danger'>
-                                <FontAwesome name='ban' /> <span>Delete account</span>
-                            </div>
-                        </Col>
+                                    </div>
+
+                                    <Table>
+                                        <tbody>
+                                        {
+                                            [1,2,3,4,5,6,7,8,9,10].map(item => (
+                                                <tr key={item}>
+                                                    <td width="20">185.27.49.66</td>
+                                                    <td><span className='it-half-opacity'>Saint Petersburg, Russia</span></td>
+                                                    <td>05:36</td>
+                                                    <td>09-06-2017</td>
+                                                </tr>
+                                            ))
+                                        }
+                                        </tbody>
+                                    </Table>
+                                </Col>
+                            ) : null
+                        }
                     </Row>
                 </div>
             </BasePage>
