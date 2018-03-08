4db76136c43de9fbfa1c128c550521b920b041c5
Fonov Sergei <ask@mounlion.com>
2018-03-08 17:38:37 +0300

    tradingview

---
+++ tradingview/src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { TVChartContainer } from './components/TVChartContainer/index';
+
+class App extends React.Component {
+
+	render() {
+
+		return (
+			<TVChartContainer />
+		);
+	}
+}
+
+export default App;
